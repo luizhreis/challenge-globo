90333409c00a22409f8e94fb5989077d070d7ae1
Luiz Reis <luizhreis.gris@gmail.com>
2024-10-29 17:01:08 -0300

    feat: Add prometheus and grafana to k8s
    Add services Prometheus and Grafana to k8s to collect metrics and provide dashboards

--- provision-k8s.sh
+++ provision-k8s.sh
@@ -12,3 +12,19 @@ kubectl apply -f k8s/api-config.yml
 kubectl apply -f k8s/api-deployment.yml
 kubectl apply -f k8s/api-service.yml
 kubectl apply -f k8s/ingress.yml
+
+# Adiciona o repositório Helm do Prometheus
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+# Instala Prometheus
+helm install prometheus prometheus-community/prometheus
+
+# Adiciona o repositório Helm do Grafana
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+# Instala Grafana
+helm install grafana grafana/grafana --set adminPassword='admin' --set service.type=LoadBalancer
+
+minikube service grafana --url
